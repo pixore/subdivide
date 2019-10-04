afb647f9db1ff6c1527f84df3b38640669f9283b
Jose Albizures <albizures3601@gmail.com>
2019-10-04 13:24:43 -0600

    remove group and divider code

--- src/utils/Container.ts
+++ src/utils/Container.ts
@@ -5,11 +5,6 @@ import { ReadOnlyState } from '../hooks/useLayout/types';
 import {
   ContainerData,
   ContainerDataUpdate,
-  DividerData,
-  Group,
-  GroupsMap,
-  DividerDataUpdate,
-  GroupUpdate,
   Vector,
 } from '../types';
 
@@ -199,11 +194,6 @@ const addId = <T extends object>(id: Id, data: T): T & { id: Id } => {
 interface SplitResult {
   originContainer: ContainerDataUpdate;
   newContainer: ContainerData;
-  divider: DividerData;
-  newGroup?: Group;
-  previousDividerUpdate?: DividerDataUpdate;
-  nextDividerUpdate?: DividerDataUpdate;
-  currentGroup?: GroupUpdate;
 }
 
 const getDelta = (
@@ -223,7 +213,6 @@ const getDelta = (
 interface AdjacentContainerUpdate {
   newContainer: ContainerDataUpdate;
   originContainer: ContainerDataUpdate;
-  dividerId: Id;
 }
 
 const getAdjacentContainers = (
@@ -231,157 +220,35 @@ const getAdjacentContainers = (
   newId: Id,
   direction: Direction,
 ): AdjacentContainerUpdate => {
-  const dividerId = Id.create();
   const { previous, next } = container;
   const newDirectionType = Direction.getType(direction);
   const isSameDirectionType = newDirectionType === container.directionType;
 
   if (Direction.isForward(direction)) {
     return {
-      dividerId,
       newContainer: {
         id: newId,
         previous: isSameDirectionType ? previous : undefined,
-        next: dividerId,
       },
       originContainer: {
         id: container.id,
-        previous: dividerId,
         next: isSameDirectionType ? next : undefined,
       },
     };
   }
 
   return {
-    dividerId,
     originContainer: {
       id: container.id,
       previous: isSameDirectionType ? previous : undefined,
-      next: dividerId,
     },
     newContainer: {
       id: newId,
-      previous: dividerId,
       next,
     },
   };
 };
 
-const getDivider = (
-  originContainer: ContainerData,
-  newContainer: ContainerData,
-  direction: Direction,
-  id: Id,
-): DividerData => {
-  const directionType = Direction.getType(direction);
-  const isVertical = Direction.isVertical(direction);
-  const previous = Direction.isForward(direction)
-    ? newContainer.id
-    : originContainer.id;
-  const next = Direction.isForward(direction)
-    ? originContainer.id
-    : newContainer.id;
-
-  if (isVertical) {
-    return {
-      id,
-      directionType,
-      previous,
-      next,
-      width: originContainer.width,
-      height: 0,
-      left: originContainer.left,
-      top: Direction.isForward(direction)
-        ? newContainer.height + newContainer.top
-        : originContainer.height + originContainer.top,
-    };
-  }
-
-  return {
-    id,
-    directionType,
-    previous,
-    next,
-    width: 0,
-    height: originContainer.height,
-    left: Direction.isForward(direction)
-      ? newContainer.width + newContainer.left
-      : originContainer.width + originContainer.top,
-    top: originContainer.top,
-  };
-};
-
-const getNewGroup = (
-  originContainer: ContainerData,
-  newDividerId: Id,
-  directionType: Direction.DirectionType,
-): Group => {
-  const { width, height, top, left } = originContainer;
-  return {
-    id: Id.create(),
-    width,
-    height,
-    top,
-    left,
-    directionType,
-    dividers: [newDividerId],
-  };
-};
-
-interface GroupsUpdate {
-  newGroup?: Group;
-  previousDividerUpdate?: DividerDataUpdate;
-  nextDividerUpdate?: DividerDataUpdate;
-  currentGroup?: GroupUpdate;
-}
-
-const getGroupsUpdate = (
-  originContainer: ContainerData,
-  newDividerId: Id,
-  direction: Direction,
-  groups: GroupsMap,
-): GroupsUpdate => {
-  const directionType = Direction.getType(direction);
-  if (originContainer.group) {
-    const isSameDirectionType = directionType === originContainer.directionType;
-
-    if (isSameDirectionType) {
-      const currentGroup = groups[originContainer.group];
-      return {
-        currentGroup: {
-          id: originContainer.group,
-          dividers: currentGroup.dividers.concat(newDividerId),
-        },
-      };
-    }
-
-    const newGroup = getNewGroup(originContainer, newDividerId, directionType);
-    const previousDividerUpdate =
-      originContainer.previous !== undefined
-        ? {
-            id: originContainer.previous,
-            next: newGroup.id,
-          }
-        : undefined;
-
-    const nextDividerUpdate =
-      originContainer.next !== undefined
-        ? {
-            id: originContainer.next,
-            previous: newGroup.id,
-          }
-        : undefined;
-
-    return {
-      newGroup,
-      previousDividerUpdate,
-      nextDividerUpdate,
-    };
-  }
-  return {
-    newGroup: getNewGroup(originContainer, newDividerId, directionType),
-  };
-};
 
 const split = (
   originContainerId: Id,
@@ -389,9 +256,10 @@ const split = (
   direction: Direction,
   to: Vector,
 ): SplitResult => {
-  const { containers, groups } = layout;
+  const { containers } = layout;
   const originContainer = containers[originContainerId];
   const id = Id.create();
+  const parent = Id.create();
   const isVertical = Direction.isVertical(direction);
   const delta = getDelta(originContainer, to, direction);
 
@@ -401,8 +269,6 @@ const split = (
   };
 
   const adjacentsUpdate = getAdjacentContainers(originContainer, id, direction);
-
-  const { dividerId } = adjacentsUpdate;
 
   const updateData: ContainerData = {
     ...originContainer,
@@ -414,35 +280,17 @@ const split = (
 
   const newData: ContainerData = {
     id,
+    parent,
     directionType: Direction.getType(direction),
     ...adjacentsUpdate.newContainer,
     ...getSizeAfterSplitFrom(updateData, initialSize, isVertical),
     ...getPositionAfterSplitFrom(originContainer, updateData, direction),
   };
 
-  const newDivider = getDivider(updateData, newData, direction, dividerId);
-
-  const groupsUpdate = getGroupsUpdate(
-    originContainer,
-    newDivider.id,
-    direction,
-    groups as GroupsMap,
-  );
-
-  if (groupsUpdate.newGroup) {
-    updateData.group = groupsUpdate.newGroup.id;
-    newData.group = groupsUpdate.newGroup.id;
-  }
-
-  if (groupsUpdate.currentGroup) {
-    newData.group = groupsUpdate.currentGroup.id;
-  }
 
   return {
     originContainer: updateData,
     newContainer: newData,
-    divider: newDivider,
-    ...groupsUpdate,
   };
 };
 
