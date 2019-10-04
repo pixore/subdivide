93b9ed8f27c87aa0f17e811fe5ee58309d91f2b0
Jose Albizures <albizures3601@gmail.com>
2019-10-04 13:28:50 -0600

    remove group and divider reducers

--- src/hooks/useLayout/reducer.ts
+++ src/hooks/useLayout/reducer.ts
@@ -1,36 +1,54 @@
-import { State, Action } from './types';
+import { State, Action, Actions, ActionType } from './types';
+import { ContainerData, ContainerDataUpdate } from '../../types';
+import Id from '../../utils/Id';
 import * as containers from './containersReducer';
-import * as dividers from './dividersReducer';
-import * as groups from './groupReducer';
+
+const generateDividers = (state: State) => {
+  return state;
+};
 
 const reducer = (state: State, action: Action): State => {
   if (Array.isArray(action)) {
     return action.reduce(reducer, state);
   }
 
-  return {
+  return generateDividers({
     containers: containers.reducer(state.containers, action),
-    dividers: dividers.reducer(state.dividers, action),
-    groups: groups.reducer(state.groups, action),
-  };
+    dividers: state.dividers,
+  });
+};
+
+const actionCreators = {
+  add: (data: ContainerData) => ({
+    type: ActionType.ADD_CONTAINER,
+    payload: data as ContainerData,
+  }),
+  update: (data: ContainerDataUpdate) => ({
+    type: ActionType.UPDATE_CONTAINER,
+    payload: data,
+  }),
+  remove: (id: Id) => ({
+    type: ActionType.UPDATE_CONTAINER,
+    payload: id,
+  }),
 };
 
 type Dispatch = (action: Action | Action[]) => void;
-export const createActions = (dispatch: Dispatch) => {
-  return {
-    containers: containers.createActions(dispatch),
-    dividers: dividers.createActions(dispatch),
-    groups: groups.createActions(dispatch),
-    batch(actions: Action[]) {
-      dispatch(actions);
-    },
-  };
-};
+const createActions = (dispatch: Dispatch): Actions => ({
+  add(data: ContainerData) {
+    dispatch(actionCreators.add(data));
+  },
+  update(data: ContainerDataUpdate) {
+    dispatch(actionCreators.update(data));
+  },
+  remove(id: Id) {
+    dispatch(actionCreators.remove(id));
+  },
+  batch(actions: Action[]) {
+    dispatch(actions);
+  },
+});
 
-export const actionCreators = {
-  containers: containers.actionCreators,
-  dividers: dividers.actionCreators,
-  groups: groups.actionCreators,
-};
+export { actionCreators, createActions };
 
 export default reducer;
