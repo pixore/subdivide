f56a76f8029155f194c05f4cef2f50a7e7b7f307
Jose Albizures <albizures3601@gmail.com>
2019-10-08 11:23:07 -0600

    feat(ids): check for used ids

--- src/utils/Id.ts
+++ src/utils/Id.ts
@@ -1,16 +1,27 @@
-
- type Id = number;
+type Id = number;
 
 let counter = 0;
+const usedIds = new Set<Id>();
 
 namespace Id {
-  export const create = (): Id => {
-    const id = counter;
-    counter = counter + 1;
+  export const addId = (id: Id | Id[]) => {
+    if (Array.isArray(id)) {
+      return id.forEach(addId);
+    }
 
-    return id
-  }
+    usedIds.add(id);
+  };
+
+  export const create = (): Id => {
+    while (usedIds.has(counter)) {
+      counter = counter + 1;
+    }
+
+    const id = counter;
+
+    addId(id);
+    return id;
+  };
 }
 
-
-export default Id
+export default Id;
