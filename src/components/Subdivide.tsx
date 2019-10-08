c4ecfff7fc172533d6fc351464962f2bbd5887aa
Jose Albizures <albizures3601@gmail.com>
2019-10-07 23:25:23 -0600

    feat(layout): support custom size and position

--- src/components/Subdivide.tsx
+++ src/components/Subdivide.tsx
@@ -2,17 +2,21 @@ import React from 'react';
 import { TinyEmitter } from 'tiny-emitter';
 import Container from './Container';
 import Divider from './Divider';
-import { Emitter, Component } from '../types';
+import { Emitter, Component, LayoutUpdate } from '../types';
 import Layout from '../layout';
 import Overlay from './Overlay';
 import Config from '../contexts/Config';
 
 interface PropTypes {
   component: Component;
+  width?: number;
+  height?: number;
+  top?: number;
+  left?: number;
 }
 
 const Subdivide: React.FC<PropTypes> = (props) => {
-  const { component } = props;
+  const { component, width, height, top, left } = props;
   const emitter = React.useMemo(() => new TinyEmitter() as Emitter, []);
   const { onLayoutChange, initialState } = Config.useConfig();
   const layout = Layout.useLayout(emitter, {
@@ -20,11 +24,23 @@ const Subdivide: React.FC<PropTypes> = (props) => {
     initialState,
   });
 
-  const [layoutRef] = layout;
+  const [layoutRef, actions] = layout;
 
   const { dividers, containers, overlay } = layoutRef.current;
 
   const overlayElement = overlay.show ? <Overlay {...overlay} /> : null;
+
+  React.useEffect(() => {
+    const { innerWidth, innerHeight } = window;
+    const layoutUpdate: LayoutUpdate = {};
+
+    layoutUpdate.width = width || innerWidth;
+    layoutUpdate.height = height || innerHeight;
+    layoutUpdate.top = top || 0;
+    layoutUpdate.left = left || 0;
+
+    actions.updateLayout(layoutUpdate);
+  }, [width, height, top, left]);
 
   return (
     <>
